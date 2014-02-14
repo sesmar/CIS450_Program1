0c78a4bc3512a47bb0c4fa03900c631065da873e
Christopher M. Sims <sesmar@gmail.com>
2014-02-13 19:14:58 -0500

    Cleaned up code a bit and added comments

--- bridge.c
+++ bridge.c
@@ -12,14 +12,21 @@ pthread_cond_t safe;
 int currentNumber = 0;
 int currentDirec = 0;
 
-//lock for current direction or direction?
+///
+//Checks if it is safe for a car to cross going the given direction
+///
+//Parameters:
+//direc: The new vehicles direction
+///
+//Output:
+//True is safe to cross; otherwise, false
+///
 bool isSafe(int direc)
 {
-	//MUTEX LOCK
-	//pthread_mutex_lock(&currentNumber_mutex);
 	bool safe = false;
 
-	if(currentNumber==0)  //always safe when bridge is empty
+	//always safe when bridge is empty
+	if(currentNumber==0)
 	{
 		safe = true;;
 	}
@@ -34,17 +41,25 @@ bool isSafe(int direc)
 		safe = false;
 	}
 
-	//pthread_mutex_unlock(&currentNumber_mutex);
 	return safe;
 }
 
+///
+//Handles vehicle arriving at the bridge.
+///
+//Parameters
+//direc: The direction the current vehicle is traveling
+//id: the "Id" of the current vehicle
+///
 void ArriveBridge(int direc, int id)
 {
 	//MUTEX LOCK
 	pthread_mutex_lock(&currentNumber_mutex);
 
+	//Check if safe to cross the bridge
 	while(!isSafe(direc))
 	{
+		//if not safe, wait.
 		pthread_cond_wait(&safe, &currentNumber_mutex);
 	}
 
@@ -61,6 +76,13 @@ void ArriveBridge(int direc, int id)
 	return;
 }
 
+///
+//Handles a vehicle crossing the bridge
+///
+//Parameters
+//direc: The direction the current vehicle is traveling
+//id: The "Id" of the current vehicle
+///
 void CrossBridge(int direc, int id)
 {
 	//MUTEX LOCK
@@ -76,16 +98,27 @@ void CrossBridge(int direc, int id)
 	return;
 }
 
+///
+//Handles a vehicle exiting the bridge
+///
+//Parameter
+//direc: The direction the current vehicle is traveling
+//id: The "Id" of the current vehicle
+/// 
 void ExitBridge(int direc, int id)
 {
 	//MUTEX LOCK
 	pthread_mutex_lock(&currentNumber_mutex);
 
+	//Decrease the number of vehicles currently on the bridge.
 	currentNumber--;
-	pthread_cond_broadcast(&safe);
 
 	//printf("Car %d traveling in direction %d has exited the bridge.\n", id, direc);
 	printf("Car %d traveling in direction %d has exited the bridge. CurrDir: %d. #Cars: %d. \n", id, direc, currentDirec, currentNumber );
+
+	//Broadcast to safe condition, that another vehicle can attempt
+	//to cross the bridge.
+	pthread_cond_broadcast(&safe);
 
 	//MUTEX UNLOCK
 	pthread_mutex_unlock(&currentNumber_mutex);
@@ -93,9 +126,17 @@ void ExitBridge(int direc, int id)
 	return;
 }
 
+///
+//The vehicle thread, simulates a vehicle crossing the bridge.
+///
+//Parameters
+//idv: The vehicle/thread id.
+/// 
 void *OneVehicle(void *idv)
 {
 	int id = *((int*)(&idv));
+
+	//Generate a random direction
 	int direc = rand() % 2;
 
 	ArriveBridge(direc, id);
@@ -105,9 +146,13 @@ void *OneVehicle(void *idv)
 	pthread_exit(NULL);
 }
 
+///
+//The main thread for the application
+///
 int main(int argc, char *argv[])
 {
 	int i;
+	
 	//Create 50 threads;
 	pthread_t threads[NUM_THREADS];
 	pthread_attr_t attr;
@@ -120,6 +165,7 @@ int main(int argc, char *argv[])
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
+	//Seed the random number generator
 	srand(time(NULL));
 
 	for(i=0; i<NUM_THREADS; i++)
